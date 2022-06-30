f51f92d963d2b26900a8415f0bb03e1907f154ef
Minhajul-Shobuj <mdminhajulislamshobuj@gmail.com>
2022-06-30 22:52:14 +0600

    second commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
